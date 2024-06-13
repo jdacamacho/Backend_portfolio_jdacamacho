6fa2ea26f78b51bb9a902e42e1911b056d367601
Julian Camacho <jdacamacho@unicauca.edu.co>
2024-06-13 12:27:54 -0500

    add: gateway interface owner and user

--- src/main/java/com/jdacamacho/hexagonal/Application/Output/ManageOwnerGatewayIntPort.java
+++ src/main/java/com/jdacamacho/hexagonal/Application/Output/ManageOwnerGatewayIntPort.java
@@ -1,5 +1,15 @@
 package com.jdacamacho.hexagonal.Application.Output;
 
-public class ManageOwnerGatewayIntPort {
-    
+import java.util.List;
+
+import com.jdacamacho.hexagonal.Domain.Models.Owner;
+
+public interface ManageOwnerGatewayIntPort {
+    public List<Owner> findAll();
+    public Owner save(Owner owner);
+    public Owner findById(long documentNumber);
+    public boolean existsById(long documentNumber);
+    public boolean existsByUsername(String username);
+    public boolean findByPropertyName(String propertyName);
+    public boolean findByNit(long nit);
 }
