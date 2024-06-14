fffda2ef7a461fa5da0b74789f6ec61fdf880ea7
Julian Camacho <jdacamacho@unicauca.edu.co>
2024-06-14 14:38:47 -0500

    add mapper

---
+++ src/main/java/com/jdacamacho/hexagonal/Infrastucture/Input/ControllerManageField/mappers/MapperFieldInfrastuctureDomainImpl.java
@@ -0,0 +1,45 @@
+package com.jdacamacho.hexagonal.Infrastucture.Input.ControllerManageField.mappers;
+
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.stereotype.Service;
+
+import com.jdacamacho.hexagonal.Domain.Objects.Field;
+import com.jdacamacho.hexagonal.Domain.Objects.Schedule;
+import com.jdacamacho.hexagonal.Infrastucture.Input.ControllerManageField.DTORequest.FieldDTORequest;
+import com.jdacamacho.hexagonal.Infrastucture.Input.ControllerManageField.DTOResponse.FieldDTOResponse;
+import com.jdacamacho.hexagonal.Infrastucture.Input.ControllerManageField.DTOResponse.ScheduleDTOResponse;
+
+@Service
+public class MapperFieldInfrastuctureDomainImpl implements MapperFieldInfrastuctureDomainInt{
+    private final ModelMapper mapper;
+
+    public MapperFieldInfrastuctureDomainImpl(ModelMapper mapper){
+        this.mapper = mapper;
+    }
+
+    @Override
+    public Field mapRequestToModel(FieldDTORequest request) {
+        return this.mapper.map(request, Field.class);
+    }
+
+    @Override
+    public FieldDTOResponse mapModelToResponse(Field model) {
+        return this.mapper.map(model, FieldDTOResponse.class);
+    }
+
+    @Override
+    public List<FieldDTOResponse> mapModelToResponse(List<Field> models) {
+        return this.mapper.map(models, new TypeToken<List<FieldDTOResponse>>(){}.getType());
+    }
+
+    @Override
+    public List<ScheduleDTOResponse> mapModelsToResponse(List<Schedule> models) {
+        return this.mapper.map(models, new TypeToken<List<ScheduleDTOResponse>>(){}.getType());
+    }
+
+    
+
+}
---
+++ src/main/java/com/jdacamacho/hexagonal/Infrastucture/Input/ControllerManageField/mappers/MapperFieldInfrastuctureDomainInt.java
@@ -0,0 +1,16 @@
+package com.jdacamacho.hexagonal.Infrastucture.Input.ControllerManageField.mappers;
+
+import java.util.List;
+
+import com.jdacamacho.hexagonal.Domain.Objects.Field;
+import com.jdacamacho.hexagonal.Domain.Objects.Schedule;
+import com.jdacamacho.hexagonal.Infrastucture.Input.ControllerManageField.DTORequest.FieldDTORequest;
+import com.jdacamacho.hexagonal.Infrastucture.Input.ControllerManageField.DTOResponse.FieldDTOResponse;
+import com.jdacamacho.hexagonal.Infrastucture.Input.ControllerManageField.DTOResponse.ScheduleDTOResponse;
+
+public interface MapperFieldInfrastuctureDomainInt {
+    Field mapRequestToModel(FieldDTORequest request);
+    FieldDTOResponse mapModelToResponse(Field model);
+    List<FieldDTOResponse> mapModelToResponse(List<Field> models);
+    List<ScheduleDTOResponse> mapModelsToResponse(List<Schedule> models);
+}
